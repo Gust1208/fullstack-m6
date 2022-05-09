b098cda8aee07e2e5a8060f8c5bbf664cbd430bb
Gustavo Gutierrez <gustavodgutierrez@hotmail.com.ar>
2022-05-08 21:41:37 -0300

    subo tarea

--- m6ud4/backend/routes/api.js
+++ m6ud4/backend/routes/api.js
@@ -1,7 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
-var cloudinary = require('cloudinary').v2;
 var nodemailer = require('nodemailer');
 
-router.get()
+
+router.get('/novedades', async function(req,res,next){
+    let novedades = await novedadesModel.getNovedades();
+
+    res.json(novedades)
+});
+
+router.post('/contacto', async (req,res)=> {
+    const mail = {
+        to: 'gustavodgutierrez@hotmail.com.ar',
+        subject: 'Contacto web',
+        html:  `${req.body.nombre} se contacto a traves de la web y quiere más 
+        información a este correo: $(req.body.email)<br>ademas hizo el siguiente 
+        comentario: ${req.body.mensaje} <br> Su teléfono es: ${req.body.telefono}`
+    }
+    const solagro = nodemailer.createSolagro({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+
+        }
+    });
+
+    await solagro.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        menssage:'Mensaje enviado'
+    });
+
+
+});
+module.exports = router;
--- m6ud4/frontend/src/pages/ContactoPage.js
+++ m6ud4/frontend/src/pages/ContactoPage.js
@@ -1,26 +1,67 @@
 import '../styles/components/pages/ContactoPage.css';
+import React, {useState} from 'react';
+import axios from 'axios';
+
+
+
 const ContactoPage = (props) => {
-    return (
+    
+
+        const initialForm = {
+            nombre: '',
+            email: '',
+            telefono: '',
+            mensaje: ''
+        }
+        const [sending, setSending] = useState(false);
+        const [msg, setMsg] = useState('');
+        const [formData, setFormData] = useState(initialForm);
+
+        const handleChange = e => {
+            const {name, value} = e.target;
+            setFormData(oldData => ({
+                ...oldData,
+                [name]: value
+            }));
+        }
+
+        const handleSubmit = async e =>{
+            e.preventDefault();
+            setMsg('');
+            setSending(true)
+            const response = await axios.post('http://localhost:3000/api/contacto', formData);
+            setSending(false);
+            setMsg(response.data.message);
+            if  (response.data.error === false){
+                setFormData(initialForm)
+            }
+        }
+
+    
+
+return (
         <main className='holder'>
             <div className='columna de contacto'>
                 <h2>Complete
                     el siguiente formulario</h2>
-                <form action="" method="" className='formulario'>
+                <form  className='formulario' onSubmit={handleSubmit}>
                     <p>
                         <label>Nombre </label>
-                        <input type='text' name='nombre' />
+                        <input type='text' name='nombre' value={formData.nombre} onChange ={handleChange} />
                     </p>
                     <p> <label>Email </label>
-                        <input type='text' name='email' />
+                        <input type='text' name='email' value={formData.email} onChange ={handleChange} />
                     </p >
                     <p ><label>Teléfono </label>
-                        <input type='text' name='teléfono' />
+                        <input type='text' name='telefono' value={formData.telefono} onChange ={handleChange} />
                     </p >
                     <p ><label>Comentario </label>
-                        <textarea name='mensaje'></textarea>
+                        <textarea name='mensaje' value={formData.mensaje} onChange ={handleChange}></textarea>
                     </p >
+                    {sending ? <p>Enviando...</p>: null}
+                    {msg ? <p>{msg}</p>: null}
                     <p className='centrar'>
-                        <input type='submit' value='Enviar' />
+                        <input type='submit' value="Enviar"  />
                     </p>
 
                 </form >
